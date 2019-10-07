5a2e884d4121af3b54a06eb840525416d5c9e2d6
Jinha2 <j_2j@naver.com>
2019-10-08 00:42:16 +0900

    initial commits

---
+++ 박진하/SearchViewer/SearchViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchViewer
+//
+//  Created by Jinha Park on 2019/10/08.
+//  Copyright © 2019 JinhaPark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
